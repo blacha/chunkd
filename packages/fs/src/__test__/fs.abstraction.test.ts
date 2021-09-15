91e4028b97e54bb06fbadd884c1680326a73e421
Blayne Chard <blayne@chard.com>
2021-09-15 21:43:55 +1200

    refactor: remove unused variable

--- packages/fs/src/__test__/fs.abstraction.test.ts
+++ packages/fs/src/__test__/fs.abstraction.test.ts
@@ -1,6 +1,6 @@
 import { FsFile } from '@chunkd/source-file';
 import o from 'ospec';
-import { FileSystemAbstraction, fsa } from '../fs.abstraction.js';
+import { FileSystemAbstraction } from '../fs.abstraction.js';
 
 export class FakeSystem extends FsFile {
   constructor(protocol = 'fake') {
@@ -11,40 +11,40 @@ export class FakeSystem extends FsFile {
 
 o.spec('FileSystemAbstraction', () => {
   o('should register new file systems', () => {
-    const fsaB = new FileSystemAbstraction();
+    const fsa = new FileSystemAbstraction();
 
     const fakeLocal = new FakeSystem('fake');
-    fsaB.register('fake://', fakeLocal);
+    fsa.register('fake://', fakeLocal);
     const fakeUnknown = new FakeSystem('unknown');
-    fsaB.register('', fakeUnknown);
+    fsa.register('', fakeUnknown);
 
-    o(fsaB.get('fake://foo').protocol).equals('fake');
-    o(fsaB.get('fake:/foo').protocol).equals('unknown');
+    o(fsa.get('fake://foo').protocol).equals('fake');
+    o(fsa.get('fake:/foo').protocol).equals('unknown');
   });
 
   o('should find file systems in order they were registered', () => {
     const fakeA = new FakeSystem('fake');
     const fakeB = new FakeSystem('fakeSpecific');
-    const fsaB = new FileSystemAbstraction();
+    const fsa = new FileSystemAbstraction();
 
-    fsaB.register('fake://', fakeA);
-    fsaB.register('fake://some-prefix-string/', fakeB);
+    fsa.register('fake://', fakeA);
+    fsa.register('fake://some-prefix-string/', fakeB);
 
-    o(fsaB.get('fake://foo').protocol).equals('fake');
-    o(fsaB.get('fake://some-prefix-string/').protocol).equals('fakeSpecific');
-    o(fsaB.get('fake://some-prefix-string/some-key').protocol).equals('fakeSpecific');
+    o(fsa.get('fake://foo').protocol).equals('fake');
+    o(fsa.get('fake://some-prefix-string/').protocol).equals('fakeSpecific');
+    o(fsa.get('fake://some-prefix-string/some-key').protocol).equals('fakeSpecific');
   });
 
   o('should order file systems by length', () => {
     const fakeA = new FakeSystem('fake');
     const fakeB = new FakeSystem('fakeSpecific');
-    const fsaB = new FileSystemAbstraction();
+    const fsa = new FileSystemAbstraction();
 
-    fsaB.register('fake://some-prefix-string/', fakeB);
-    fsaB.register('fake://', fakeA);
+    fsa.register('fake://some-prefix-string/', fakeB);
+    fsa.register('fake://', fakeA);
 
-    o(fsaB.get('fake://foo').protocol).equals('fake');
-    o(fsaB.get('fake://some-prefix-string/').protocol).equals('fakeSpecific');
-    o(fsaB.get('fake://some-prefix-string/some-key').protocol).equals('fakeSpecific');
+    o(fsa.get('fake://foo').protocol).equals('fake');
+    o(fsa.get('fake://some-prefix-string/').protocol).equals('fakeSpecific');
+    o(fsa.get('fake://some-prefix-string/some-key').protocol).equals('fakeSpecific');
   });
 });
