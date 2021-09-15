a128127b6d3f97df6fc2c4128269d1fcaa61b6f5
Blayne Chard <blayne@chard.com>
2021-09-15 18:36:15 +1200

    refactor: remove make folder option

--- packages/source-file/src/file.fs.ts
+++ packages/source-file/src/file.fs.ts
@@ -68,9 +68,9 @@ export class FsFile implements FileSystem<SourceFile> {
     return this.head(filePath).then((f) => f != null);
   }
 
-  async write(filePath: string, buf: Buffer | Readable, makeFolder = true): Promise<void> {
+  async write(filePath: string, buf: Buffer | Readable): Promise<void> {
     const folderPath = path.dirname(filePath);
-    if (makeFolder) await fs.promises.mkdir(folderPath, { recursive: true });
+    await fs.promises.mkdir(folderPath, { recursive: true });
     try {
       if (Buffer.isBuffer(buf)) {
         await fs.promises.writeFile(filePath, buf);
