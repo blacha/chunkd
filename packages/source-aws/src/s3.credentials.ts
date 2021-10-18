22781a2a9ddd934e7439dce7c29266cd12b8fe17
Blayne Chard <blayne@chard.com>
2021-10-19 11:09:32 +1300

    fix(source-aws): include duration unit in description

--- packages/source-aws/src/s3.credentials.ts
+++ packages/source-aws/src/s3.credentials.ts
@@ -2,13 +2,20 @@ import { Credentials } from 'aws-sdk/lib/credentials.js';
 import aws from 'aws-sdk/lib/core.js';
 
 export class AwsCredentials {
-  static defaultRoleDuration = 3600;
+  static DefaultRoleDurationSeconds = 3600;
   static cache: Map<string, Credentials> = new Map();
 
-  static role(roleArn: string, externalId?: string, duration?: number): Credentials {
-    duration = duration ?? AwsCredentials.defaultRoleDuration;
+  /**
+   * Create temporary credentials for a AWS Role
+   * @param roleArn Role to assume
+   * @param externalId Role ExternalId if it exists
+   * @param durationSeconds Number of seconds to assume the role for default 3_600
+   * @returns Credentials to use
+   */
+  static role(roleArn: string, externalId?: string, durationSeconds?: number): Credentials {
+    durationSeconds = durationSeconds ?? AwsCredentials.DefaultRoleDurationSeconds;
 
-    const roleKey = `role::${roleArn}::${externalId}::${duration}`;
+    const roleKey = `role::${roleArn}::${externalId}::${durationSeconds}`;
     let existing = AwsCredentials.cache.get(roleKey);
     if (existing == null) {
       existing = new aws.ChainableTemporaryCredentials({
@@ -16,7 +23,7 @@ export class AwsCredentials {
           RoleArn: roleArn,
           ExternalId: externalId,
           RoleSessionName: 'fsa-' + Math.random().toString(32) + '-' + Date.now(),
-          DurationSeconds: duration,
+          DurationSeconds: durationSeconds,
         },
       });
       AwsCredentials.cache.set(roleKey, existing);
