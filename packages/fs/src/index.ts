6b59c598138ee8a1f2b244af1e2c9fa1e3ca36e4
Blayne Chard <blayne@chard.com>
2022-05-23 16:46:20 +1200

    fix(fs): ensure http is registered by default in web contexts

--- packages/fs/src/index.ts
+++ packages/fs/src/index.ts
@@ -1,2 +1,9 @@
-export { FileSystemAbstraction, fsa } from './fs.abstraction.js';
+import { FsHttp } from '@chunkd/source-http';
+import { fsa } from './fs.abstraction.js';
+
 export { FsHttp } from '@chunkd/source-http';
+export { FileSystemAbstraction, fsa } from './fs.abstraction.js';
+
+const fsHttp = new FsHttp();
+fsa.register('http://', fsHttp);
+fsa.register('https://', fsHttp);
