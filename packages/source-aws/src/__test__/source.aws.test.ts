d146aa1d24a450c2eb0ee50fffbbb51068420dd2
Blayne Chard <blayne@chard.com>
2024-08-22 12:17:37 +1200

    fix(source-aws): prevent s3 client from being logged when errors are thrown (#1475)
    ### Motivation
    
    The `source` object is thrown when errors happen such as when a read
    fails. If this error is logged then the `soruce.client` is also logged
    and logging the entire S3 client is huge.
    
    ### Modification
    
    Prevent the S3 object from being visible for loggers

--- packages/source-aws/src/__test__/source.aws.test.ts
+++ packages/source-aws/src/__test__/source.aws.test.ts
@@ -7,4 +7,11 @@ describe('SourceAwsS3', () => {
   it('should create s3 links from string url', () => {
     assert.equal(new SourceAwsS3('s3://foo/bar.txt').url.href, 's3://foo/bar.txt');
   });
+
+  it('should not expose "client" for logging', () => {
+    const source = new SourceAwsS3('s3://foo/bar.txt');
+    const keys = Object.keys(source);
+    assert.equal(keys.includes('client'), false);
+    assert.notEqual(source.client.config, null);
+  });
 });
--- packages/source-aws/src/index.ts
+++ packages/source-aws/src/index.ts
@@ -51,6 +51,8 @@ export class SourceAwsS3 implements Source {
 
   constructor(url: URL | string, client: S3Client = new S3Client({})) {
     this.url = typeof url === 'string' ? new URL(url) : url;
+    // S3 clients are very large and if this object gets logged the log is very very large
+    Object.defineProperty(this, 'client', { enumerable: false });
     this.client = client;
   }
 
