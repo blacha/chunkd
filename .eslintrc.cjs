ee319a5d7161252987e5480bb39c86bb62bfb6ef
Blayne Chard <blayne@chard.com>
2026-03-21 21:54:40 +1300

    refactor: drop eslint/prettier

--- .eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-const cfg = {
-  ...require('@linzjs/style/.eslintrc.cjs'),
-};
-
-const testOverrides = cfg.overrides.find((ovr) => ovr.files.find((f) => f.includes('.test.ts')));
-testOverrides.rules['@typescript-eslint/no-floating-promises'] = 'off';
-
-module.exports = cfg;
